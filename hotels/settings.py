7ea6e9b2ea9d97d6135658ccf99cd4115b4a1491
vijay gholve <153069430+vijayholve@users.noreply.github.com>
2024-06-06 18:28:58 -0400

    Update settings.py

--- hotels/settings.py
+++ hotels/settings.py
@@ -144,7 +144,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'vijaygholve77v@gmail.com'
-EMAIL_HOST_PASSWORD = 'uoyn xeho visj fblr'
+EMAIL_HOST_PASSWORD = ''
 
 
 # setting.py
