61586b8805efa919ee95f3668d8f1fcd36d1e311
vijay gholve <vijaygholve77c@gmail.com>
2024-06-11 18:48:29 -0400

    updated

---
+++ .history/accounts/views_20240611184118.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from .models import UserProfile
+from rooms.models import Room,Booking
+
+def User_profile_fun(request,pk):
+    user=User.objects.get(id=pk)
+    restaurant=user.restaurants_set.all()
+    bookeds=user.booking_set.all()
+    rooms=user.room_set.all()
+    orders=user.orders_set.all()
+    if user.userprofile:
+        profile=user.userprofile
+    content={"user":user,"profile":profile,"restaurant":restaurant,"booked":bookeds,
+             "rooms":rooms,""}
+    return render(request,"accounts/userProfile.html",content)
+def update_profile(request,pk):
+    profile=UserProfile.objects.get(id=pk)
+    if request.method == "POST":
+        
+        return _extracted_from_update_profile_5(request, profile)
+    content={"profile":profile}
+    return render(request,"accounts/update_profile.html",content)
+
+
+# TODO Rename this here and in `update_profile`
+def _extracted_from_update_profile_5(request, profile):
+    username=request.POST.get("username")
+    city=request.POST.get("city")
+    ProfilePicture=request.FILES.get("image")
+    dob=request.POST.get("dob")
+
+    if ProfilePicture:
+        profile.profilePicture=ProfilePicture
+    if dob:
+        profile.dateOfBirth=dob
+    profile.city=city
+    profile.user.username=username
+    profile.save()
+    return  redirect("profile",pk=profile.user.id)
+
+def create_profile(request):
+    users=User.objects.all()
+    if request.method == "POST":
+        return _extracted_from_create_profile_6(request, users)
+    content={"users":users}
+    return render(request,"accounts/createprofile.html",content)    
+def _extracted_from_create_profile_6(request,users):
+    
+    city=request.POST.get("city")
+    ProfilePicture=request.FILES.get("image")
+    dob=request.POST.get("dob")
+    user=request.user
+    profile, created = UserProfile.objects.get_or_create(user=user)
+    profile.profilePicture = ProfilePicture
+    profile.dateOfBirth = dob
+    profile.city = city
+    profile.save()
+    # if ProfilePicture:
+    #     profile.profilePicture=ProfilePicture
+    # if dob:
+    #     profile.dateOfBirth=dob
+    # profile.city=city
+    # user=request.user
+    profile.save()
+    return  redirect("profile",pk=profile.user.id)
---
+++ .history/base/models_20240611175647.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User
+class hotel(models.Model):
+    name=models.CharField(max_length=200)
+    city=models.CharField(max_length=200)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    created=models.DateTimeField(auto_now=True)
+    updated=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+class restaurants(models.Model):
+    restaurantName=models.CharField(max_length=200)
+    locations=models.CharField(max_length=200)
+    image=models.FileField(upload_to="restaurant_images",null="True",blank=True)
+    hotel=models.ForeignKey(hotel,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.restaurantName
+    class Meta:
+        ordering=["-id"]
+class dish(models.Model):
+    dishName=models.CharField(max_length=200)
+    description=models.CharField( max_length=200)
+    price=models.IntegerField(blank=True,null=True)
+    restaurants=models.ForeignKey(restaurants,on_delete=models.SET_NULL,null=True,blank= True)
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="dishes")
+    dishImage=models.FileField(upload_to="images/",max_length=250,null=True,blank=True)
+    hotel=models.ForeignKey(hotel,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.dishName
+class orders(models.Model):
+    restaurants=models.ForeignKey(restaurants,on_delete=models.CASCADE)
+    dish=models.OneToOneField(dish,on_delete=models.CASCADE,related_name="dish")
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    create_at=models.DateTimeField(auto_now_add=True)
+    location=models.CharField(max_length=200,default)
+    delivery_charges=models.IntegerField(default=50)
+    total_charges=models.IntegerField()
+    def  __str__(self):
+        return f"{self.restaurants.restaurantName}"
