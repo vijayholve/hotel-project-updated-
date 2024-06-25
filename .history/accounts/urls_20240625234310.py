d30d20a39f8ad2a4319162b50892fb0f0d9c174e
vijay gholve <vijaygholve77c@gmail.com>
2024-06-26 02:00:14 +0530

    up

---
+++ .history/accounts/urls_20240625234310.py
@@ -0,0 +1,7 @@
+from django.urls import path,include
+from . import views
+urlpatterns = [
+    path("<str:pk>/",views.User_profile_fun,name="profile"),
+    path("profile-update/<str:pk>/",views.update_profile,name="profile-update"),
+    path()
+]
